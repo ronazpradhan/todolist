f929d6fbf774b0d2a80141de0c8fad2272cdeb43
ronazpradhan <rownaz49@gmail.com>
2025-07-07 13:48:22 +0545

    re-named

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// https://vite.dev/config/
 export default defineConfig({
+    base: '/todolist/', // Add this line
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/todolist"
+    // ... any other configurations you have
 });
