6d28d3c2c5b7df640678f478f09738ce59579fea
ronazpradhan <rownaz49@gmail.com>
2025-07-07 14:39:52 +0545

    Fix: Ensure base path is correct for GitHub Pages assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-// vite.config.ts
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/todolist/', // Add this line
   plugins: [react()],
